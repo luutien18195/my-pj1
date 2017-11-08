14e488a3b0bb916161690a397d3f5fb77a73787e
luutien18195 <luutien18195@gmail.com>
2017-11-08 18:16:53 +0700

    finish login

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/help", to: "static_pages#help"
-  get "/login", to: "static_pages#login"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
 
   post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
 
   resources :users
 end
